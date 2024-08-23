33ff9846c20b83efb96f18aa312dce28f539239d
jai <jai@pearlorganisation.com>
2024-08-23 18:27:17 +0530

    udpates

--- Backend/src/controller/visualization.js
+++ Backend/src/controller/visualization.js
@@ -1,8 +1,88 @@
+import _ from "lodash";
 import dotenv from "dotenv";
 dotenv.config();
 
 const apiKeysString = process.env.API_KEYS;
 const apiKeys = JSON.parse(apiKeysString);
+
+const processData = (data, params) => {
+
+  let cleanedArr = [];
+
+  const result = data.map((project) => {
+    project[params.y1Axis.value] = removeCommaFromNum(
+      project[params.y1Axis.value]
+    );
+    project[params.y2Axis.value] = removeCommaFromNum(
+      project[params.y2Axis.value]
+    );
+    return project;
+  });
+  let tempArr = [...result];
+
+  let groupedResult = _.groupBy(tempArr, "dept_name");
+  _.keys(groupedResult).forEach((e) => {
+    let tempObj = {};
+    tempObj = groupedResult[`${e}`];
+    cleanedArr.push(tempObj);
+  });
+
+  let calcArr = cleanedArr.map((dept) => {
+    const projects = dept;
+    const y1Total = projects.reduce(
+      (sum, project) => sum + parseFloat(project[params.y1Axis.value]),
+      0
+    );
+    const y2Total = projects.reduce(
+      (sum, project) => sum + parseFloat(project[params.y2Axis.value]),
+      0
+    );
+
+    const projectCount = projects.length;
+
+    const y1 = y1Total / projectCount;
+    const y2 = y2Total / projectCount;
+
+    return {
+      xAxis: Number(projects[0][params.xAxis.value]) - 543,
+      y1,
+      y2,
+    };
+  });
+
+  return calcArr || [];
+};
+
+const removeCommaFromNum = (item) => {
+  return item.replace(/\,/g, "");
+};
+
+const generateChartData = (data, params) => {
+  let chartStructure = {
+    labels: [],
+    datasets: [
+      {
+        label: `${params.y1Axis.label}`,
+        backgroundColor: "red",
+        data: [], // y axis
+      },
+      {
+        label: `${params.y2Axis.label}`,
+        backgroundColor: "orange",
+        data: [], // y axis
+      },
+    ],
+  };
+
+  data.forEach((item) => {
+    chartStructure.labels.push(item.xAxis);
+    // first column
+    chartStructure.datasets[0].data.push(item.y1);
+    // second column
+    chartStructure.datasets[1].data.push(item.y2);
+  });
+  return chartStructure;
+};
 
 const findProjectsData = async (data) => {
   let projectsLimit = 900;
@@ -23,13 +103,11 @@ const findProjectsData = async (data) => {
         method: "GET",
       };
 
-      console.log(url);
-
       const response = await fetch(url, options);
       const res = await response.json(); // Convert response body to JSON
 
       if (res.message !== "API rate limit exceeded" && i < apiKeysLength) {
-        allResult.push({ result: res.result });
+        allResult = [...allResult, ...res.result];
         break;
       } else {
         allResult = { status: false, message: "API rate limit exceeded" };
@@ -38,12 +116,21 @@ const findProjectsData = async (data) => {
     }
     year -= 1;
   }
-  return allResult; // Return the JSON data
+
+  let finalData = [];
+
+  if (allResult.length > 0) {
+    const calcArr = processData(allResult, data);
+    finalData = generateChartData(calcArr, data);
+  }
+
+  return finalData; // Return the JSON data
 };
 
 export const getVisualizationData = async (req, res) => {
   try {
     let data = req.body;
+    console.log(data);
     data.year = Number(data?.yearsFrom?.value);
 
     const projectRes = await findProjectsData(data);
--- Frontend/src/pages/VisualizationForm.jsx
+++ Frontend/src/pages/VisualizationForm.jsx
@@ -4,12 +4,11 @@ import Select from "react-select";
 import Skeleton from "react-loading-skeleton";
 import { Toaster, toast } from "sonner";
 import axios from "axios";
-import BarChart from '../components/BarChart'
+import BarChart from "../components/BarChart";
 
 const VisualizationForm = () => {
   const [years, setYears] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [apiData, setApiData] = useState(null);
   const [chartData, setChartData] = useState(null);
   const chartRef = useRef(null);
 
@@ -33,95 +32,8 @@ const VisualizationForm = () => {
   };
 
   useEffect(() => {
-    // if (localStorage.getItem("apiData")) {
-    //   localStorage.clear();
-    // }
     populateYears();
   }, []);
-
-  const processData = (data) => {
-    let tempArr = [];
-    let cleanedArr = [];
-    for (let i = 0; i < data?.length; i++) {
-      const result = data[i].result.map((project) => {
-        project.project_money = removeCommaFromNum(project.project_money);
-        project.sum_price_agree = removeCommaFromNum(project.sum_price_agree);
-        return project;
-      });
-      tempArr = [...tempArr, ...result];
-    }
-    let groupedResult = Object.groupBy(tempArr, ({ budget_year }) => budget_year);
-    Object.keys(groupedResult).forEach((e) => {
-      let tempObj = {};
-      tempObj = groupedResult[`${e}`];
-      cleanedArr.push(tempObj);
-    });
-
-    let calcArr = cleanedArr.map((dept) => {
-      const projects = dept;
-      const totalReferencePrice = projects.reduce(
-        (sum, project) => sum + parseFloat(project.price_build),
-        0
-      );
-      const totalWinningPrice = projects.reduce(
-        (sum, project) => sum + parseFloat(project.sum_price_agree),
-        0
-      );
-
-      const projectCount = projects.length;
-
-      const y1 = totalReferencePrice / projectCount;
-      const y2 = totalWinningPrice / projectCount;
-
-      return {
-        xAxis: Number(projects[0][`budget_year`]) - 543,
-        y1,
-        y2,
-      };
-    });
-
-    return calcArr || [];
-  };
-
-  const removeCommaFromNum = (item) => {
-    return item.replace(/\,/g, "");
-  };
-
-  const generateChartData = (data) => {
-    let chartStructure = {
-      labels: [],
-      datasets: [
-        {
-          label: "y1", // x axis
-          backgroundColor: "red",
-          data: [], // y axis
-        },
-        {
-          label: "y2", // x axis
-          backgroundColor: "orange",
-          data: [], // y axis
-        },
-      ],
-    };
-
-    data.forEach((item) => {
-      chartStructure.labels.push(item.xAxis);
-      // first column
-      chartStructure.datasets[0].data.push(item.y1);
-      // second column
-      chartStructure.datasets[1].data.push(item.y2);
-    });
-    console.log(chartStructure)
-    setChartData(chartStructure);
-  };
-
-  useEffect(() => {
-    if (!apiData && apiData?.length <= 0) return;
-
-    const calcArr = processData(apiData?.data)
-
-    generateChartData(calcArr);
-  }, [apiData]);
 
   const onSubmit = (data) => {
     if (Number(data?.yearsTo?.value) - Number(data?.yearsFrom?.value) > 10) {
@@ -144,15 +56,14 @@ const VisualizationForm = () => {
       return;
     }
 
+    setChartData(null);
     setIsLoading(true);
     axios
       .post(`${import.meta.env.VITE_API_URL}/projects/visualization`, data)
       .then((res) => {
-        console.log(res)
-        let resData = { data: res.data, formData: data };
-        setApiData(resData);
-        if (resData?.data?.length > 0) {
-          toast.success("Projects Data Found", {
+        if (res?.data) {
+          setChartData(res.data);
+          toast.success("Visualized Data Found", {
             style: {
               background: "green",
               color: "white",
@@ -172,7 +83,6 @@ const VisualizationForm = () => {
           behavior: "smooth",
         });
 
-        localStorage.setItem("apiData", JSON.stringify(resData));
         setIsLoading(false);
       })
       .catch((err) => {
@@ -196,22 +106,22 @@ const VisualizationForm = () => {
       });
   };
 
-  const xAxesData = [
-    { value: "Year", label: "Year" },
-    { value: "⁠month", label: "⁠month" },
-    { value: "⁠department name", label: "⁠department name" },
-    { value: "⁠company name", label: "⁠company name" },
-    { value: "⁠purchase method", label: "⁠purchase method" },
-    { value: "⁠sub purchase method", label: "⁠sub purchase method" },
-    { value: "⁠project status", label: "⁠project status" },
-    { value: "⁠location", label: "⁠location" },
-    { value: "⁠budget range", label: "⁠budget range" },
+  const xAxisData = [
+    { label: "Year", value: "budget_year" },
+    { label: "⁠month", value: "budget_year" },
+    { label: "⁠department name", value: "dept_name" },
+    { label: "⁠company name", value: "winner" },
+    { label: "⁠purchase method", value: "⁠purchase_method_name" },
+    // { label: "⁠sub purchase method", value: "⁠sub purchase method" },
+    { label: "⁠project status", value: "⁠project_status" },
+    { label: "⁠location", value: "⁠location" },
+    { label: "⁠budget range", value: "⁠budget range" },
   ];
 
-  const yAxesData = [
-    { value: "Project budget value", label: "Project budget value" },
-    { value: "⁠winning bid value", label: "⁠winning bid value" },
-    { value: "⁠project id", label: "⁠project id" },
+  const yAxisData = [
+    { value: "project_money", label: "Project budget value" },
+    { value: "sum_price_agree", label: "⁠winning bid value" },
+    { value: "project_id", label: "⁠project id" },
     { value: "⁠lead time", label: "⁠lead time" }, // (project award date - project announce date)
   ];
 
@@ -326,15 +236,15 @@ const VisualizationForm = () => {
         <div className="w-1/2">
           <div className="mb-1">
             <label
-              htmlFor="x_axes"
+              htmlFor="xAxis"
               className="block mb-1 text-sm font-medium text-gray-90"
             >
-              X Axes
+              X Axis
             </label>
             <Controller
-              name="xAxes"
+              name="xAxis"
               control={control}
-              render={({ field }) => <Select {...field} options={xAxesData} />}
+              render={({ field }) => <Select {...field} options={xAxisData} />}
             />
           </div>
         </div>
@@ -342,21 +252,21 @@ const VisualizationForm = () => {
         <div className="grid grid-cols-2 gap-2">
           <div className="mb-1">
             <label
-              htmlFor="y_axes_1"
+              htmlFor="y1Axis"
               className="block mb-1 text-sm font-medium text-gray-90"
             >
               Y Axes1
             </label>
             <Controller
-              name="yAxes1"
+              name="y1Axis"
               control={control}
-              render={({ field }) => <Select {...field} options={yAxesData} />}
+              render={({ field }) => <Select {...field} options={yAxisData} />}
             />
           </div>
 
           <div className="mb-1">
             <label
-              htmlFor="y_axes_1_group_by"
+              htmlFor="groupBy1"
               className="block mb-1 text-sm font-medium text-gray-90"
             >
               Group By
@@ -374,21 +284,21 @@ const VisualizationForm = () => {
         <div className="grid grid-cols-2 gap-2">
           <div className="mb-1">
             <label
-              htmlFor="y_axes_2"
+              htmlFor="y2Axis"
               className="block mb-1 text-sm font-medium text-gray-90"
             >
               Y Axes2
             </label>
             <Controller
-              name="yAxes2"
+              name="y2Axis"
               control={control}
-              render={({ field }) => <Select {...field} options={yAxesData} />}
+              render={({ field }) => <Select {...field} options={yAxisData} />}
             />
           </div>
 
           <div className="mb-1">
             <label
-              htmlFor="y_axes_2_group_by"
+              htmlFor="groupBy2"
               className="block mb-1 text-sm font-medium text-gray-90"
             >
               Group By
@@ -426,7 +336,7 @@ const VisualizationForm = () => {
         </div>
       </form>
 
-      <div ref={chartRef}>
+      <div ref={chartRef} className="py-6 w-full md:w-auto">
         {isLoading && <Skeleton count={6} className="  w-[55vw] h-[40px]" />}
         {chartData && <BarChart data={chartData} />}
       </div>
